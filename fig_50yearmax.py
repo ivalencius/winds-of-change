a4c1963e994501af5d66a37925b31c2a345bcabf
Ilan Valencius <valencig@bc.edu>
2024-01-19 12:36:27 -0700

    using cesm2 large ensemble

--- fig_50yearmax.py
+++ fig_50yearmax.py
@@ -6,18 +6,44 @@ from cartopy.mpl.ticker import LatitudeFormatter, LongitudeFormatter
 import nc_time_axis
 
 def fig_50yearmax(cesm2, forecast='near'):
-
-    fig, axes = plt.subplots(figsize=(25,10), ncols=2, nrows=2, constrained_layout=True, subplot_kw={"projection":ccrs.PlateCarree()})
-
-    for key, ax in zip(['ssp126','ssp245', 'ssp370', 'ssp585'], axes.flat):
-        ds = cesm2[key+'.50yrmax']
-        da = ds.isel(member_id=0).sfcWind.sel(forecast=forecast)
-        # Plot mean for this species
-        fg = da.plot.contourf(ax=ax, vmin=5, vmax=20,  cmap='plasma', add_colorbar=False, levels=11)
-        # cb = plt.colorbar(fg, orientation="vertical", pad=0.05, extend='both')
-        # cb.set_label(label='Decade NSWS Trend', size=18, weight='bold')
-        # cb.ax.tick_params(labelsize=16)
-        ax.set_title(key)
+    # levels = 10
+    
+    fig, axes = plt.subplots(figsize=(20,5), ncols=2, nrows=1, constrained_layout=True, subplot_kw={"projection":ccrs.PlateCarree()})
+    ds = cesm2['h1.50yrWSPDSRFMX']
+    
+    # Plot 1 -> mean climatology
+    da = ds.mean('member_id').sel(forecast=forecast).WSPDSRFMX
+    fg = da.plot.contourf(
+        ax=axes[0],
+        cmap='plasma',
+        add_colorbar=False,
+        vmin=18,
+        vmax=30,
+        levels=11
+    )
+    axes[0].set_title('Extreme 50-year gust', fontsize=22)
+    # Add colorbar
+    cb = plt.colorbar(fg, ax=axes[0], orientation="vertical", extend='both', fraction=0.046, pad=0.04)
+    cb.set_label(label=f'Extreme 50-year Gust [m/s]', size=18, weight='bold')
+    cb.ax.tick_params(labelsize=16)
+    
+    # Plot 2 -> model agreement (using std right now)
+    da = ds.std('member_id').sel(forecast=forecast).WSPDSRFMX
+    fg = da.plot(
+        ax=axes[1],
+        vmin=0,
+        vmax=8,
+        cmap='cividis',
+        add_colorbar=False,
+        levels=9,
+    )
+    axes[1].set_title('Disagreement among members', fontsize=22)
+    # Add colobar
+    cb = plt.colorbar(fg, ax=axes[1], orientation="vertical", extend='max', fraction=0.046, pad=0.04)
+    cb.set_label(label=f'Std in 50-year Gust [m/s]', size=18, weight='bold')
+    cb.ax.tick_params(labelsize=16)
+    
+    for ax in axes.flat:
         ax.add_feature(cfeature.STATES)
         ax.coastlines()
         gl = ax.gridlines(
@@ -31,8 +57,6 @@ def fig_50yearmax(cesm2, forecast='near'):
         lat_formatter = LatitudeFormatter()
         ax.xaxis.set_major_formatter(lon_formatter)
         ax.yaxis.set_major_formatter(lat_formatter)
-        # Add title
-        ax.set_title(key, fontsize=22)
         # Increase the ticksize
         gl.xlabel_style = {'size': 16, 'color': 'k', 'rotation':30, 'ha':'right'}
         gl.ylabel_style = {'size': 16, 'color': 'k', 'weight': 'normal'}
@@ -40,8 +64,8 @@ def fig_50yearmax(cesm2, forecast='near'):
     # cax = fig.add_axes([0.05, 0.05, 0.9, 0.1])
     # Clear settings from scienceplot package
     # cax.clear()
-    cb = plt.colorbar(fg, ax=axes.ravel().tolist(), orientation="vertical", extend='both', fraction=0.046, pad=0.04)
-    cb.set_label(label=f'Extreme 50-year Gust', size=18, weight='bold')
-    cb.ax.tick_params(labelsize=16)
+    # cb = plt.colorbar(fg, ax=axes.ravel().tolist(), orientation="vertical", extend='both', fraction=0.046, pad=0.04)
+    #cb.set_label(label=f'Extreme 50-year Gust', size=18, weight='bold')
+    #cb.ax.tick_params(labelsize=16)
     fig.suptitle(f'Extreme 50-year Gust ({forecast}-term)', fontsize=30)
     plt.show()
--- fig_50yearmax_trend.py
+++ fig_50yearmax_trend.py
@@ -8,51 +8,31 @@ import numpy as np
 
 def fig_50yearmax_trend(cesm2):
 
-    fig, ax = plt.subplots(figsize=(15,8), ncols=1, nrows=1, constrained_layout=True)
-    data = [[], [], [], []]
-    for i, key in enumerate(['ssp126', 'ssp245', 'ssp370', 'ssp585']):
-        ds = cesm2[key+'.50yrmax']
-        for f in ['near', 'mid', 'far']:
-            da = ds.isel(member_id=0).sfcWind.sel(forecast=f)
-            val = da.values.flatten()
-            good_val = val[np.isfinite(val)]
-            data[i].append(good_val)
-        # for p in [1, 2]:
-        #     data[i][p] = data[i][p]-data[i][p-1]
-        # for x in range(len(data[i])):
-        #     data[i][x] = data[i][x][np.isfinite(data[i][x])]
-    positions = np.array([0, 0.25, 0.5])
-
-    for d in data:
-        bp = ax.boxplot(
-            d,
-            positions=positions,
-            patch_artist=True
+    fig, ax = plt.subplots(figsize=(6,4), ncols=1, nrows=1, constrained_layout=True)
+    ds = cesm2['h1.50yrWSPDSRFMX']
+    da = ds.mean({'lat', 'lon'}).load()
+    x_axis = ['near', 'mid', 'far']
+    
+    for i in range(len(da.member_id)):
+        y = da.isel(member_id=i).WSPDSRFMX.values
+        # Standardize y by first value
+        y = (y-y[0])/y[0]
+        plt.plot(
+            x_axis,
+            y,
+            color='k',
+            alpha=0.3,
+            marker='o',
+            markersize=1
         )
-        for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
-            plt.setp(bp[element], color='k')
-        for patch, c in zip(bp['boxes'], ['#4EF5E2', '#F5D34E', '#F54FEB']):
-            patch.set(facecolor=c)
-        positions += 1
-
-    # set style for the axes
-    labels = ['ssp126', 'ssp245', 'ssp370', 'ssp585']
-    ax.set_xticks(np.arange(0.25, len(labels)+0.25), labels=labels)
-    # Add lines to seperate projections
-    ymin, ymax = ax.get_ylim()
-    ymax = 25
-    ax.set_ylim(ymin, ymax)
-    for i in range(3):
-        ax.vlines(0.75+i, ymin, ymax, color='gray', linestyle='--')
-
     # Increase the ticksize
-    plt.xticks(fontsize=14)
+    plt.xticks(ticks=x_axis, labels=['Near-term\n(2021-2040)', 'Mid-term\n(2041-2060)', 'Far-term\n(2081-2100)'],fontsize=14)
     plt.yticks(fontsize=14)
 
     # Add some labels and increase the size
     plt.xlabel('Projection', fontsize=16)
-    plt.ylabel('Exteme 50-year Gust [m/s]', fontsize=16)
+    plt.ylabel('Exteme 50-year Gust [% change from near-term]', fontsize=16)
 
-    plt.title('Extreme 50-year Gust from Generalized Extreme Value Distribution', fontsize=30)
+    plt.title('Extreme 50-year Gust from Generalized Extreme Value Distribution', fontsize=22)
     
     plt.show()
