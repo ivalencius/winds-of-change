9b06e0f007ebe1992629346e57e41da92ee53851
Ilan Valencius <valencig@bc.edu>
2024-01-23 11:11:38 -0700

    more cesm2le2 plots

--- fig_50yearmax_trend.py
+++ fig_50yearmax_trend.py
@@ -8,7 +8,7 @@ import numpy as np
 
 def fig_50yearmax_trend(cesm2):
 
-    fig, ax = plt.subplots(figsize=(6,4), ncols=1, nrows=1, constrained_layout=True)
+    fig, ax = plt.subplots(figsize=(7,5), ncols=1, nrows=1, constrained_layout=True)
     ds = cesm2['h1.50yrWSPDSRFMX']
     da = ds.mean({'lat', 'lon'}).load()
     x_axis = ['near', 'mid', 'far']
@@ -20,18 +20,20 @@ def fig_50yearmax_trend(cesm2):
         plt.plot(
             x_axis,
             y,
-            color='k',
-            alpha=0.3,
+            # color='green',
+            alpha=0.5,
             marker='o',
-            markersize=1
+            markersize=3
         )
+    # Add zero line
+    ax.hlines(0, 'near', 'far', linestyle='--', color='k')
     # Increase the ticksize
     plt.xticks(ticks=x_axis, labels=['Near-term\n(2021-2040)', 'Mid-term\n(2041-2060)', 'Far-term\n(2081-2100)'],fontsize=14)
     plt.yticks(fontsize=14)
 
     # Add some labels and increase the size
-    plt.xlabel('Projection', fontsize=16)
-    plt.ylabel('Exteme 50-year Gust [% change from near-term]', fontsize=16)
+    ax.set_xlabel('Projection', fontsize=16)
+    ax.set_ylabel('Exteme 50-year Gust [\% change from near-term]', fontsize=12)
 
     plt.title('Extreme 50-year Gust from Generalized Extreme Value Distribution', fontsize=22)
     
